0d1cb6bd4132743f9aae4002990e04ac984b2977
Lucemans <luc@lucemans.nl>
2024-08-22 17:50:10 +0200

    Initial Site

---
+++ web/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable no-undef */
+import { Link } from '@tanstack/react-router';
+
+const LOGIN_URL = 'http://localhost:3000/login';
+
+export const Navbar = () => {
+    return (
+        <div className="w-full bg-white border-b h-8 flex items-center justify-between">
+            <div className="h-full flex space-x-2">
+                <div className="font-semibold text-base pl-4 pr-4 hover:bg-black/10 border-r h-full flex items-center">
+                    sample.rip
+                </div>
+                <div className="h-full flex items-center">
+                    {[
+                        ['/', 'Home'],
+                        ['/sounds', 'Sounds'],
+                    ].map(([path, name]) => (
+                        <Link
+                            key={path}
+                            to={path}
+                            className="[&.active]:font-bold hover:bg-black/10 py-1 px-2"
+                        >
+                            {name}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+            <a
+                href={'/'}
+                className="h-full border-l px-2 py-0.5 flex items-center hover:bg-black/10"
+            >
+                Login
+            </a>
+        </div>
+    );
+};
---
+++ web/src/routes/__root.tsx
@@ -0,0 +1,13 @@
+import { createRootRoute, Outlet } from '@tanstack/react-router';
+
+import { Navbar } from '../components/Navbar';
+
+export const Route = createRootRoute({
+    component: () => (
+        <>
+            <Navbar />
+            <Outlet />
+            {/* <TanStackRouterDevtools /> */}
+        </>
+    ),
+});
